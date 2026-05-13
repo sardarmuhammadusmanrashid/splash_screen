a9fcd08ccd2709c0062acc0ae9c592ab75f53039
Usman-Rashid <usman.rashid@aitomation.com>
2026-05-14 00:24:05 +0500

    Initial commit
    Generated by react-native@0.85.3

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Zouj 🤝</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#E8503A',
+  },
+});
